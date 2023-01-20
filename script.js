9157bad44e2971390945e51426da9cb88e856f04
sosopyl <122552117+sosopyl@users.noreply.github.com>
2023-01-20 10:04:55 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+// задание 1
+// let text = document.querySelector('p')
+// let inp = document.querySelector('input')
+// inp.addEventListener('blur', func)
+// function func(){
+//     text.textContent = text.textContent + ' ' + inp.value
+// }
+
+// задание 2
+// elems = document.querySelectorAll('.aaa')
+// let text = document.querySelector('p')
+// let btn = document.querySelector('#button')
+
+// btn.addEventListener('click', func)
+
+// function func(){
+// let sum = 0;
+// for(let elem of elems){
+//     sum += +elem.value
+// }
+// text.textContent = sum
+// }
+
+
+// задание 3
+// задание 4
+// задание 5
+// задание 6
+// задание 7
+// задание 8
+// задание 9
+// задание 10
+// задание 11
+// задание 12
+// задание 13
+// задание 14
+// задание 15
+// задание 16
+// задание 17
+// задание 18
+// задание 19
+// задание 20
+// задание 21
+// задание 22
+// задание 23
+// задание 24
