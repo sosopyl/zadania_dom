4e28ecff17d7cf0e6341529a904a97eba3936234
sosopyl <122552117+sosopyl@users.noreply.github.com>
2023-01-22 20:27:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -23,10 +23,66 @@
 
 
 // задание 3
+//  let elem = document.querySelector('input')
+//  elem.addEventListener('blur', function(){
+//     let arr = elem.value.split('');
+//     let sum = 0;
+//     for(let num of arr){
+//         sum += +num
+//     }
+//     elem.value = sum
+//  })
+
 // задание 4
+
+// let inp = document.querySelector('input')
+// inp.addEventListener('blur', function(){
+//     let arr = inp.value.split(',')
+//     let sum = 0;
+//     for(let elem of arr){
+//         sum += +elem
+//     }
+//     let res = sum / arr.length
+//     inp.value = res
+// })
+
 // задание 5
+// let fio = document.querySelector('#fio')
+// let name = document.querySelector('#name')
+// let surname = document.querySelector('#surname')
+// let otch = document.querySelector('#otch')
+// fio.addEventListener('blur', func)
+// function func(){
+//     let arr = fio.value.split(" ")
+//     name.value = arr[0];
+//     surname.value = arr[1]
+//     otch.value = arr[2]
+// }
+
 // задание 6
+// let inp = document.querySelector('input')
+// inp.addEventListener('blur', func)
+// function func(){
+// let arr = inp.value.split(' ')
+// let str = ''
+// for ( let elem of arr){
+//     let elem1 = elem[0].toUpperCase() + elem.slice(1)
+//     str += elem1
+//     str += " "
+// }
+// inp.value = str
+// }
+
 // задание 7
+// let inp = document.querySelector('input')
+// let text = document.querySelector('p')
+// inp.addEventListener('blur', func)
+// function func(){
+// let arr = inp.value.split('')
+// let res = arr.length
+// console.log(res)
+// text.textContent = res
+// }
 // задание 8
 // задание 9
 // задание 10
